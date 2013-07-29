2bc0d18394cda16c6f4f498c63133bd849d0ad41
Eric Gonzalez <ericrgon@us.ibm.com>
2013-07-28 20:13:25 -0500

    Updated nest to include away state.

--- jobs/nest.rb
+++ jobs/nest.rb
@@ -6,7 +6,7 @@ nest_password = ENV['NEST_PASSWORD']
 SCHEDULER.every '1m', :first_in => 0 do |job|
 	nest = NestThermostat::Nest.new({email: nest_user,password: nest_password})
 	first_nest = nest.status["shared"][nest.device_id]
-	temp = nest.temperature; 
+	temp = nest.temperature.to_i; 
 	away = nest.away
 	state = "off"	
 	leaf_src = ""
@@ -23,5 +23,9 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 		leaf_src = "assets/nest_leaf_trans.png"
 	end
 
-	send_event('nest', { temp: temp.to_i , state: state, away: away, leaf: leaf_src })
+	if(away)
+		temp = "Away"
+	end
+
+	send_event('nest', { temp: temp , state: state, away: away, leaf: leaf_src })
 end
